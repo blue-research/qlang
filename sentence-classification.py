b1877588397ba3314b8b866db20ded3eeffda5d5
079035 <sroot079@gmail.com>
2024-05-17 15:18:29 -0400

    using this script for experiments from now on

--- sentence-classification.py
+++ sentence-classification.py
@@ -2,9 +2,21 @@
 import numpy as np
 import warnings
 import os
+from datasets import load_dataset
+from lambeq import BobcatParser
+from lambeq import RemoveCupsRewriter
+import discopy
+from lambeq import Rewriter
+from lambeq import AtomicType, IQPAnsatz, MPSAnsatz, Sim14Ansatz, Sim15Ansatz, SpiderAnsatz, StronglyEntanglingAnsatz, Symbol, TensorAnsatz
+from lambeq.backend.tensor import Dim
+from pytket.extensions.qiskit import AerBackend
+from lambeq import TketModel
+from lambeq import BinaryCrossEntropyLoss
+from lambeq import QuantumTrainer, SPSAOptimizer
+from lambeq import Dataset
+# print(lambeq.__version__)
 warnings.filterwarnings("ignore")
 os.environ["TOKENIZERS_PARALLELISM"] = "false"
-
 
 def read_data(filename):
     labels, sentences = [], []
@@ -15,8 +27,7 @@ def read_data(filename):
             sentences.append(line[1:].strip())
     return labels, sentences
 
-from lambeq import Rewriter
-def diagram_rewritting(diagrams): #(data):
+def diagram_rewriting(diagrams): # (data):
     #diagrams = parser.sentences2diagrams(data)
     rewriter = Rewriter(['prepositional_phrase', 'determiner'])
     rewritten_diagrams = [rewriter(diagram) for diagram in diagrams] 
@@ -30,12 +41,7 @@ train_labels, train_data = read_data('datasets/mc_train_data.txt')
 dev_labels, dev_data = read_data('datasets/mc_dev_data.txt')
 test_labels, test_data = read_data('datasets/mc_test_data.txt')
 
-import discopy
-#print(lambeq.__version__)
-
-from lambeq import BobcatParser
-
-parser = BobcatParser(verbose='text')
+parser = BobcatParser(verbose='text', device=0) # device=-1 for CPU
 
 raw_train_diagrams = parser.sentences2diagrams(train_data)
 raw_train_diagrams[0].draw()
@@ -44,45 +50,37 @@ raw_dev_diagrams[0].draw()
 raw_test_diagrams = parser.sentences2diagrams(test_data)
 raw_test_diagrams[0].draw()
 
-#from lambeq import remove_cups
-#from lambeq import Rewriter
 #rewriter = Rewriter(['prepositional_phrase', 'determiner'])
 #rewritten_diagram = rewriter(diagram)
 
-print("Before removing cups")
+# train_diagrams = raw_train_diagrams
+# dev_diagrams = raw_dev_diagrams
+# test_diagrams = raw_test_diagrams
 
-raw_train_diagrams[0].draw()
-# train_diagrams = [remove_cups(diagram) for diagram in raw_train_diagrams]
-# print("After removing cups1")
+remove_cups = RemoveCupsRewriter()
+train_diagrams = [remove_cups(diagram) for diagram in raw_train_diagrams]
+dev_diagrams = [remove_cups(diagram) for diagram in raw_dev_diagrams]
+test_diagrams = [remove_cups(diagram) for diagram in raw_test_diagrams]
+
+# train_diagrams = diagram_rewriting(raw_train_diagrams) #[diagram.normal_form() for diagram in raw_train_diagrams]
+# dev_diagrams = diagram_rewriting(raw_dev_diagrams) #[diagram.normal_form()for diagram in raw_dev_diagrams]
+# test_diagrams = diagram_rewriting(raw_test_diagrams) #[diagram.normal_form() for diagram in raw_test_diagrams]
 # train_diagrams[0].draw()
 
-train_diagrams = diagram_rewritting(raw_train_diagrams) #[diagram.normal_form() for diagram in raw_train_diagrams]
-print("After removing cups2")
-train_diagrams[0].draw()
-
-
-dev_diagrams = diagram_rewritting(raw_dev_diagrams) #[diagram.normal_form()for diagram in raw_dev_diagrams]
-
-dev_diagrams[0].draw()
-
-test_diagrams = diagram_rewritting(raw_test_diagrams) #[diagram.normal_form() for diagram in raw_test_diagrams]
-test_diagrams[0].draw()
-
-
-from lambeq import AtomicType, IQPAnsatz
-
-ansatz = IQPAnsatz({AtomicType.NOUN: 1, AtomicType.SENTENCE: 1},
-                   n_layers=1, n_single_qubit_params=3)
+# ansatz = IQPAnsatz({AtomicType.NOUN: 1, AtomicType.SENTENCE: 1},
+#                    n_layers=1, n_single_qubit_params=3)
+# ansatz = StronglyEntanglingAnsatz({AtomicType.NOUN: 1, AtomicType.SENTENCE: 1}, 
+#                                   n_layers=2, n_single_qubit_params=3)
+ansatz = Sim14Ansatz({AtomicType.NOUN: 1, AtomicType.SENTENCE: 1},
+                   n_layers=2, n_single_qubit_params=3)
+# ansatz = Sim15Ansatz({AtomicType.NOUN: 1, AtomicType.SENTENCE: 1},
+#                    n_layers=2, n_single_qubit_params=3)
 
 train_circuits = [ansatz(diagram) for diagram in train_diagrams]
 train_circuits[0].draw(figsize=(9, 12))
 dev_circuits =  [ansatz(diagram) for diagram in dev_diagrams]
-
 test_circuits = [ansatz(diagram) for diagram in test_diagrams]
-test_circuits[0].draw(figsize=(9, 12))
-
-from pytket.extensions.qiskit import AerBackend
-from lambeq import TketModel
+# test_circuits[0].draw(figsize=(9, 12))
 
 all_circuits = train_circuits+dev_circuits+test_circuits
 
@@ -94,14 +92,9 @@ backend_config = {
 }
 
 model = TketModel.from_diagrams(all_circuits, backend_config=backend_config)
-
 #loss = lambda y_hat, y: -np.sum(y * np.log(y_hat)) / len(y)  # binary cross-entropy loss
-from lambeq import BinaryCrossEntropyLoss
 loss = BinaryCrossEntropyLoss()
-
 acc = lambda y_hat, y: np.sum(np.round(y_hat) == y) / len(y) / 2  # half due to double-counting
-
-from lambeq import QuantumTrainer, SPSAOptimizer
 
 EPOCHS = 120
 BATCH_SIZE = 30
@@ -118,8 +111,6 @@ trainer = QuantumTrainer(
     seed=0
 )
 
-from lambeq import Dataset
-
 train_dataset = Dataset(
             train_circuits,
             train_labels,
@@ -128,7 +119,19 @@ train_dataset = Dataset(
 val_dataset = Dataset(dev_circuits, dev_labels, shuffle=False)
 trainer.fit(train_dataset, val_dataset) #, logging_step=1)
 
-model = TketModel.from_checkpoint('classifier.pickle', backend_config=backend_config)
-model.save("classifier.pickle")
+# model = TketModel.from_checkpoint('./models/classifier-Sim15-rewrite.pickle', backend_config=backend_config)
+model.save("./models/classifier-Sim14-no-cups.pickle")
 
+def measure_score(model):
+        test_pred = model.get_diagram_output(test_circuits).tolist()
+        total = 0
+        for i in range(len(test_pred)):
+            if test_pred[i][0]>=0.5:
+                test_pred[i][0],test_pred[i][1]=1.0,0.0
+            else:
+                test_pred[i][0],test_pred[i][1]=0.0,1.0
+            if test_pred[i][0]==test_labels[i][0] and test_pred[i][1]==test_labels[i][1]:
+                total+=1
+        return total/len(test_labels)
 
+print("Model Accuracy: ", measure_score(model))
